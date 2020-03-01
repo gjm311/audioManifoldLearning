1726f5396c29d28d6ff1e23d57735506aa3e10f7
gjm311 <gjm311@nyu.edu>
2020-03-01 14:21:47 +0200

    update to rtf samples

--- code/sourceLocalization/functions/randSourcePos.m
+++ code/sourceLocalization/functions/randSourcePos.m
@@ -3,9 +3,9 @@ function sourcePos = randSourcePos(numRand, roomSize, radius, reference)
         sourcePos = rand(numRand, 3);
         sourcePos(:,1) = sourcePos(:,1)*roomSize(:,1);
         sourcePos(:,2) = sourcePos(:,2)*roomSize(:,2);
-        sourcePos(:,3) = ones(numRand,1)*roomSize(3);
+        sourcePos(:,3) = ones(numRand,1)*reference(3);
     else
         sourcePos = reference + (rand(numRand, 3)-.5)*radius;
-        sourcePos(:,3) = ones(numRand,1)*roomSize(3);
+        sourcePos(:,3) = ones(numRand,1)*reference(3);
     end
 end
